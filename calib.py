da8de084003824165f696710a2a182f21c2702be
EstevaoMGomes <estevao.gomes04@gmail.com>
2023-12-15 17:38:02 +0000

    fit channel0 finnished

--- calib.py
+++ calib.py
@@ -38,38 +38,26 @@ plt.show()
 
 # For the calibration we will use the data from the TT_21 file
 
-plt.figure()
-plt.plot(TT21_Chn0, label='Chnannel 0')
-plt.legend()
-plt.title('TT_21')
-plt.xlabel('Channel')
-plt.yscale('log')
-plt.ylabel('Counts')
-plt.xlim(500, 700)
-plt.ylim(1, 1e3)
-plt.savefig('TT_21_Chn0.png')
-plt.show()
-
 # Gaussian fit for the peaks (soma 3 de gaussianas)
 def gaussian(x, a1, x01, sigma1, a2, x02, sigma2, a3, x03, sigma3, c):
     return a1*np.exp(-(x-x01)**2/(2*sigma1**2)) + a2*np.exp(-(x-x02)**2/(2*sigma2**2)) + a3*np.exp(-(x-x03)**2/(2*sigma3**2)) + c
 
 # Peaks
-x = np.array(range(500, 701))
-print(x)
-y = TT21_Chn0[500:700]
-print(y)
-popt, popcov = curve_fit(gaussian, x,y)
+x = np.array(range(545, 650))
+y = TT21_Chn0[545:650]
+# curve fit
+popt, pcov = curve_fit(gaussian, x, y, p0=[90, 556, 5, 70, 593, 5, 70, 627, 5, 0])
 
 plt.figure()
 plt.plot(TT21_Chn0, label='Chnannel 0')
 plt.plot(x, gaussian(x, *popt), label='Chnannel 0 fit')
+plt.title('Fit channel 0')
 plt.legend()
 plt.xlabel('Channel')
 plt.yscale('log')
 plt.ylabel('Counts')
 plt.xlim(500, 700)
-plt.ylim(1, 1e3)
+plt.ylim(1, 1e2)
 plt.savefig('TT_21_Chn0.png')
 plt.show()
 
