a3b6f982355e868bdbdfa80d81e99f28be78e929
pedrocurvo <95446270+pedrocurvo@users.noreply.github.com>
2023-12-16 17:08:34 +0000

    Organize Paths

--- analise.py
+++ analise.py
@@ -2,6 +2,24 @@ import numpy as np
 import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from pathlib import Path
+
+# Define paths
+# data
+DATA_PATH = Path('data')
+# data folders
+TT_20_PATH = DATA_PATH / 'TT_20' / 'UNFILTERED'
+TT_21_PATH = DATA_PATH / 'TT_21' / 'UNFILTERED'
+TT_22_PATH = DATA_PATH / 'TT_22' / 'UNFILTERED'
+TT_23_PATH = DATA_PATH / 'TT_23' / 'UNFILTERED'
+TT_24_PATH = DATA_PATH / 'TT_24' / 'UNFILTERED'
+TT_25_PATH = DATA_PATH / 'TT_25' / 'UNFILTERED'
+TT_26_PATH = DATA_PATH / 'TT_26' / 'UNFILTERED'
+TT_27_PATH = DATA_PATH / 'TT_27' / 'UNFILTERED'
+# image
+IMAGE_PATH = Path('images')
+IMAGE_PATH.mkdir(exist_ok=True, parents=True)
+
 
 # Function to read the data from the file
 def reader (name, Chn):
@@ -18,44 +36,44 @@ def reader (name, Chn):
 # Reading the data files
 
 # TT_20
-TT20_Chn0 = reader('data/TT_20/UNFILTERED/CH0@N6781_21198_Espectrum_TT_20_20231207_141538.n42', 0)
-TT20_Chn1 = reader('data/TT_20/UNFILTERED/CH1@N6781_21198_Espectrum_TT_20_20231207_141538.n42', 1)
-TT20_Chn2 = reader('data/TT_20/UNFILTERED/CH2@N6781_21198_Espectrum_TT_20_20231207_141538.n42', 2)
+TT20_Chn0 = reader(TT_20_PATH / 'CH0@N6781_21198_Espectrum_TT_20_20231207_141538.n42', 0)
+TT20_Chn1 = reader(TT_20_PATH /'CH1@N6781_21198_Espectrum_TT_20_20231207_141538.n42', 1)
+TT20_Chn2 = reader(TT_20_PATH /'CH2@N6781_21198_Espectrum_TT_20_20231207_141538.n42', 2)
 
 # TT_21
-TT21_Chn0 = reader('data/TT_21/UNFILTERED/CH0@N6781_21198_Espectrum_TT_21_20231207_143401.n42', 0)
-TT21_Chn1 = reader('data/TT_21/UNFILTERED/CH1@N6781_21198_Espectrum_TT_21_20231207_143401.n42', 1)
-TT21_Chn2 = reader('data/TT_21/UNFILTERED/CH2@N6781_21198_Espectrum_TT_21_20231207_143401.n42', 2)
+TT21_Chn0 = reader(TT_21_PATH / 'CH0@N6781_21198_Espectrum_TT_21_20231207_143401.n42', 0)
+TT21_Chn1 = reader(TT_21_PATH / 'CH1@N6781_21198_Espectrum_TT_21_20231207_143401.n42', 1)
+TT21_Chn2 = reader(TT_21_PATH / 'CH2@N6781_21198_Espectrum_TT_21_20231207_143401.n42', 2)
 
 # TT_22
-TT22_Chn0 = reader('data/TT_22/UNFILTERED/CH0@N6781_21198_Espectrum_TT_22_20231207_144117.n42', 0)
-TT22_Chn1 = reader('data/TT_22/UNFILTERED/CH1@N6781_21198_Espectrum_TT_22_20231207_144117.n42', 1)
-TT22_Chn2 = reader('data/TT_22/UNFILTERED/CH2@N6781_21198_Espectrum_TT_22_20231207_144117.n42', 2)
+TT22_Chn0 = reader(TT_22_PATH / 'CH0@N6781_21198_Espectrum_TT_22_20231207_144117.n42', 0)
+TT22_Chn1 = reader(TT_22_PATH / 'CH1@N6781_21198_Espectrum_TT_22_20231207_144117.n42', 1)
+TT22_Chn2 = reader(TT_22_PATH / 'CH2@N6781_21198_Espectrum_TT_22_20231207_144117.n42', 2)
 
 # TT_23
-TT23_Chn0 = reader('data/TT_23/UNFILTERED/CH0@N6781_21198_Espectrum_TT_23_20231207_144752.n42', 0)
-TT23_Chn1 = reader('data/TT_23/UNFILTERED/CH1@N6781_21198_Espectrum_TT_23_20231207_144752.n42', 1)
-TT23_Chn2 = reader('data/TT_23/UNFILTERED/CH2@N6781_21198_Espectrum_TT_23_20231207_144752.n42', 2)
+TT23_Chn0 = reader(TT_23_PATH / 'CH0@N6781_21198_Espectrum_TT_23_20231207_144752.n42', 0)
+TT23_Chn1 = reader(TT_23_PATH / 'CH1@N6781_21198_Espectrum_TT_23_20231207_144752.n42', 1)
+TT23_Chn2 = reader(TT_23_PATH / 'CH2@N6781_21198_Espectrum_TT_23_20231207_144752.n42', 2)
 
 # TT_24
-TT24_Chn0 = reader('data/TT_24/UNFILTERED/CH0@N6781_21198_Espectrum_TT_24_20231207_145454.n42', 0)
-TT24_Chn1 = reader('data/TT_24/UNFILTERED/CH1@N6781_21198_Espectrum_TT_24_20231207_145454.n42', 1)
-TT24_Chn2 = reader('data/TT_24/UNFILTERED/CH2@N6781_21198_Espectrum_TT_24_20231207_145454.n42', 2)
+TT24_Chn0 = reader(TT_24_PATH / 'CH0@N6781_21198_Espectrum_TT_24_20231207_145454.n42', 0)
+TT24_Chn1 = reader(TT_24_PATH / 'CH1@N6781_21198_Espectrum_TT_24_20231207_145454.n42', 1)
+TT24_Chn2 = reader(TT_24_PATH / 'CH2@N6781_21198_Espectrum_TT_24_20231207_145454.n42', 2)
 
 # TT_25
-TT25_Chn0 = reader('data/TT_25/UNFILTERED/CH0@N6781_21198_Espectrum_TT_25_20231207_150004.n42', 0)
-TT25_Chn1 = reader('data/TT_25/UNFILTERED/CH1@N6781_21198_Espectrum_TT_25_20231207_150004.n42', 1)
-TT25_Chn2 = reader('data/TT_25/UNFILTERED/CH2@N6781_21198_Espectrum_TT_25_20231207_150004.n42', 2)
+TT25_Chn0 = reader(TT_25_PATH / 'CH0@N6781_21198_Espectrum_TT_25_20231207_150004.n42', 0)
+TT25_Chn1 = reader(TT_25_PATH / 'CH1@N6781_21198_Espectrum_TT_25_20231207_150004.n42', 1)
+TT25_Chn2 = reader(TT_25_PATH / 'CH2@N6781_21198_Espectrum_TT_25_20231207_150004.n42', 2)
 
 # TT_26
-TT26_Chn0 = reader('data/TT_26/UNFILTERED/CH0@N6781_21198_Espectrum_TT_26_20231207_152550.n42', 0)
-TT26_Chn1 = reader('data/TT_26/UNFILTERED/CH1@N6781_21198_Espectrum_TT_26_20231207_152550.n42', 1)
-TT26_Chn2 = reader('data/TT_26/UNFILTERED/CH2@N6781_21198_Espectrum_TT_26_20231207_152550.n42', 2)
+TT26_Chn0 = reader(TT_26_PATH / 'CH0@N6781_21198_Espectrum_TT_26_20231207_152550.n42', 0)
+TT26_Chn1 = reader(TT_26_PATH / 'CH1@N6781_21198_Espectrum_TT_26_20231207_152550.n42', 1)
+TT26_Chn2 = reader(TT_26_PATH / 'CH2@N6781_21198_Espectrum_TT_26_20231207_152550.n42', 2)
 
 # TT_27
-TT27_Chn0 = reader('data/TT_27/UNFILTERED/CH0@N6781_21198_Espectrum_TT_27_20231207_160722.n42', 0)
-TT27_Chn1 = reader('data/TT_27/UNFILTERED/CH1@N6781_21198_Espectrum_TT_27_20231207_160722.n42', 1)
-TT27_Chn2 = reader('data/TT_27/UNFILTERED/CH2@N6781_21198_Espectrum_TT_27_20231207_160722.n42', 2)
+TT27_Chn0 = reader(TT_27_PATH / 'CH0@N6781_21198_Espectrum_TT_27_20231207_160722.n42', 0)
+TT27_Chn1 = reader(TT_27_PATH / 'CH1@N6781_21198_Espectrum_TT_27_20231207_160722.n42', 1)
+TT27_Chn2 = reader(TT_27_PATH / 'CH2@N6781_21198_Espectrum_TT_27_20231207_160722.n42', 2)
 
 
 # Plotting the data
@@ -70,7 +88,7 @@ plt.title('TT_20')
 plt.yscale('log')
 plt.xlabel('Channel')
 plt.ylabel('Counts')
-plt.savefig('TT_20.png')    
+plt.savefig(IMAGE_PATH/'TT_20.png')    
 plt.show()
 
 # TT_21
@@ -83,7 +101,7 @@ plt.title('TT_21')
 plt.yscale('log')
 plt.xlabel('Channel')
 plt.ylabel('Counts')
-plt.savefig('TT_21.png')
+plt.savefig(IMAGE_PATH/'TT_21.png')
 plt.show()
 
 # TT_22
@@ -96,7 +114,7 @@ plt.title('TT_22')
 plt.yscale('log')
 plt.xlabel('Channel')
 plt.ylabel('Counts')
-plt.savefig('TT_22.png')
+plt.savefig(IMAGE_PATH/'TT_22.png')
 plt.show()
 
 # TT_23
@@ -109,7 +127,7 @@ plt.title('TT_23')
 plt.yscale('log')
 plt.xlabel('Channel')
 plt.ylabel('Counts')
-plt.savefig('TT_23.png')
+plt.savefig(IMAGE_PATH/'TT_23.png')
 plt.show()
 
 # TT_24
@@ -122,11 +140,12 @@ plt.title('TT_24')
 plt.yscale('log')
 plt.xlabel('Channel')
 plt.ylabel('Counts')
-plt.savefig('TT_24.png')
+plt.savefig(IMAGE_PATH/'TT_24.png')
 plt.show()
 
 # TT_25
 plt.figure()
+plt.subplot(1, 3, 1)
 plt.plot(TT25_Chn0, label='Chnannel 0')
 plt.plot(TT25_Chn1, label='Channel 1')
 plt.plot(TT25_Chn2, label='Channel 2')
@@ -135,7 +154,7 @@ plt.title('TT_25')
 plt.yscale('log')
 plt.xlabel('Channel')
 plt.ylabel('Counts')
-plt.savefig('TT_25.png')
+plt.savefig(IMAGE_PATH/'TT_25.png')
 plt.show()
 
 # TT_26
@@ -148,7 +167,7 @@ plt.title('TT_26')
 plt.yscale('log')
 plt.xlabel('Channel')
 plt.ylabel('Counts')
-plt.savefig('TT_26.png')
+plt.savefig(IMAGE_PATH/'TT_26.png')
 plt.show()
 
 # TT_27
@@ -161,7 +180,7 @@ plt.title('TT_27')
 plt.yscale('log')
 plt.xlabel('Channel')
 plt.ylabel('Counts')
-plt.savefig('TT_27.png')
+plt.savefig(IMAGE_PATH/'TT_27.png')
 plt.show()
 
 # Calibration
@@ -176,7 +195,7 @@ plt.xlabel('Channel')
 plt.yscale('log')
 plt.ylabel('Counts')
 plt.xlim(500, 700)
-plt.savefig('TT_21_Chn0.png')
+plt.savefig(IMAGE_PATH/'TT_21_Chn0.png')
 plt.show()
 
 # Gaussian fit for the peaks (soma 3 de gaussianas)
@@ -196,7 +215,7 @@ plt.xlabel('Channel')
 plt.ylabel('Counts')
 plt.yscale('log')
 plt.xlim(500, 700)
-#plt.savefig('TT_21_Chn0_fit.png')
+#plt.savefig(IMAGE_PATH/'TT_21_Chn0_fit.png')
 plt.show()
 
 
